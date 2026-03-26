36b8e8426cc120de00b8bcc804d5f9bda72068ff
fedor <amogusggy@internet.ru>
2026-03-26 18:10:20 +0300

    something, that i should call fixes

--- src/nebula/app.py
+++ src/nebula/app.py
@@ -5,7 +5,7 @@ from concurrent.futures import ThreadPoolExecutor
 from typing import List, Optional, Dict, Any, Union
 from pathlib import Path
 
-from nebula import HTMLResponse
+from .responses import HTMLResponse
 
 from .router import Router
 from .ws import WebSocket
